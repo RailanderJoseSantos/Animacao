4efd0665ebd44f8adf0e5eab9e9d09c58b67bedf
Railander Jose <railandergama@gmail.com>
2019-10-03 07:49:30 -0300

    Initial commit

---
+++ Assets/Programacao/Sensor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour {
+
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider outro)
+    {
+        animator.SetBool("entrou", true);
+    }
+    private void OnTriggerExit(Collider outro)
+    {
+        animator.SetBool("entrou", false);
+    }
+}
